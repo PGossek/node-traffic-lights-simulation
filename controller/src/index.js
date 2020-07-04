4286ff79e1907f7cd9ba5012056aec0971a53542
PGossek <piotr.goss@op.pl>
2020-07-04 03:09:20 +0200

    Add Vue.js to show traffic lights actual colors (#3)

--- controller/src/index.js
+++ controller/src/index.js
@@ -12,44 +12,77 @@ const port = 8080;
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 let totalWsNumber = 0;
 let greenColors = 0;
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-        console.log(`There is new traffic light with ${message} id`);
-        ws.id = message;
+function sendDataToVue(wss, ws, data) {
+    wss.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN && client.id === undefined) {
+            client.send(JSON.stringify(data));
+        }
+    });
+}
+
+function collectDataForVue(wss) {
+    const dataMapArray = [];
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN && client.id !== undefined) {
+            dataMapArray.push({id: client.id, color: client.color});
+        }
+    });
+    return dataMapArray;
+}
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        if(message === 'VueWantsData') {
+            ws.send(JSON.stringify(collectDataForVue(wss)));
+        }
+        else {
+            console.log(`There is new traffic light with ${message} id`);
+            ws.id = message;
+            totalWsNumber++;
+            const color = choseLight.getLightColor('none', totalWsNumber, greenColors);
+            ws.color = color;
+            ws.send(color);
+            if (ws.color === choseLight.colors.GREEN) {
+                greenColors++;
+            }
+            sendDataToVue(wss, ws, [{id: ws.id, color: ws.color}]);
+            setInterval(() => {
+                if (ws.readyState === WebSocket.OPEN) {
+                    if (ws.color === choseLight.colors.GREEN) {
+                        greenColors--;
+                    }
+                    const color = choseLight.getLightColor(ws.color, totalWsNumber, greenColors);
+                    ws.color = color;
+                    ws.send(color);
+                    if (ws.color === choseLight.colors.GREEN) {
+                        greenColors++;
+                    }
+                    sendDataToVue(wss, ws, [{id: ws.id, color: ws.color}]);
+                }
+            }, 20000);
+        }
     });
     ws.on('close', () => {
-        totalWsNumber--;
         if (ws.color === choseLight.colors.GREEN) {
             greenColors--;
         }
         console.log(`WebSocket for ${ws.id}  was closed`);
+        if (ws.id === undefined) {
+            ws.terminate();
+            return;
+        }
+        sendDataToVue(wss, ws,[{id: ws.id, color: 'Connection closed'}]);
+        totalWsNumber--;
+        ws.terminate();
     });
-    totalWsNumber++;
-    const color = choseLight.getLightColor('none', totalWsNumber, greenColors);
-    ws.color = color;
-    ws.send(color);
-    if (ws.color === choseLight.colors.GREEN) {
-        greenColors++;
-    }
-    setInterval(() => {
-        if (ws.color === choseLight.colors.GREEN) {
-            greenColors--;
-        }
-        const color = choseLight.getLightColor(ws.color, totalWsNumber, greenColors);
-        ws.color = color;
-        ws.send(color);
-        if (ws.color === choseLight.colors.GREEN) {
-            greenColors++;
-        }
-    }, 20000);
 });
 
-server.listen(port, function(err) {
+server.listen(port, (err) => {
     if (err) {
         throw err;
     }
